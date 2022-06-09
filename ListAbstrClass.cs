436712ef996c4dce3474a4b60ea00f84744451d3
1 <nasgul321@yandex.ru>
2022-06-09 10:45:05 +0700

    Aleksandr_Test

---
+++ ListAbstrClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomatoTimer
+{
+    //список абстрактнах классов для WinForms TomatoTier
+    //абстр класс интервал 
+    public abstract class In_Interval
+    {
+    }
+    //абстр класс для таймера
+    public abstract class In_TomatoTamer
+    {
+    }
+    //абстр класс для создания таймера(типа фабрика) 
+    public abstract class In_CustomTT
+    {
+    }
+    //абстр класс для секундомера 
+    public abstract class In_StopWatch
+    {
+    }
+}
